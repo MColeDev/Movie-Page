dbf1ad16980db898b1f92442223785d753177fb7
RandnarC <mattcoleart@yahoo.com>
2017-10-12 16:35:04 -0500

    Add files via upload

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -6,7 +6,7 @@ import re
 main_page_head = '''
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>MoviePage</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -51,7 +51,7 @@ main_page_head = '''
             width: 100%;
             left: 0;
             top: 0;
-            background-color: white;
+            background-color: grey;
         }
     </style>
     <script type="text/javascript" charset="utf-8">
@@ -105,7 +105,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">MoviePage</a>
           </div>
         </div>
       </div>
